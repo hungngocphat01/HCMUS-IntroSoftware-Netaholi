8b33a6cab86839bf9edbdd0d083b42cc8a44d2b0
hungngocphat01 <hungngocphat01@gmail.com>
2021-12-13 00:28:07 +0700

    Settings change (add new apps)

--- netaholi/settings.py
+++ netaholi/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'courses'
+    'courses',
+    'manager',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [

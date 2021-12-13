2bab070163c4506c4500aac79001c5652210c3e1
hungngocphat01 <hungngocphat01@gmail.com>
2021-12-13 17:01:07 +0700

    Edit allowed hosts

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '[::1]']
 
 # Application definition
 

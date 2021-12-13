b553b0292d452e5cd155adcb2902716faa0f8ace
hungngocphat01 <hungngocphat01@gmail.com>
2021-12-13 17:02:36 +0700

    Edit allowed hosts

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS =  ['netaholi.herokuapp.com', 'localhost', '127.0.0.1', '[::1]']
 
 # Application definition
 
